e4600b7453041c06c2c7e84e546414034f97d07e
Armando Dorantes Bollain <bollain@gmail.com>
2017-10-19 18:31:31 -0700

    Coming soon..

---
+++ js/script.js
@@ -0,0 +1,27 @@
+window.onload = function(){
+  var covfefeisms = function(){
+    var copy = [
+      "Big League",
+      "Yuge",
+      "Believe me",
+      "Bad Hombres Roasts",
+      "Win",
+      "Big Beautiful Roasts",
+      "Covfefe",
+      "Coming Soon..."
+    ];
+    var node = document.getElementsByTagName("p")[0];
+    var dafuq = node[0];
+    console.log(node);
+
+    var index = 0;
+    var step = function() {
+      node.textContent = copy[index % copy.length];
+      index++;
+      setTimeout(step, 1500);
+    };
+    step();
+  };
+
+  covfefeisms();
+};
